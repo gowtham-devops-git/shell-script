b252c9a17176a96e8c3ab6ad1c6ffee485291564
GopalakrishnaRatnala <rlgk97016@gmail.com>
2026-03-06 12:10:59 +0530

    all1

--- variables2.sh
+++ variables2.sh
@@ -2,9 +2,9 @@
 
 echo "please enter you username:"
 
-read  $USERNAME 
+read -s $USERNAME 
 
-echo "username entered: $USERNAME"
+echo "username entered:" $USERNAME
 
 echo "Please enter your password::"
 

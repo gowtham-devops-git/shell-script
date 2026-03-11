587ce48b5bb42770474b726623c714b1463c2758
GopalakrishnaRatnala <rlgk97016@gmail.com>
2026-03-11 10:11:52 +0530

    all

--- conditions.sh
+++ conditions.sh
@@ -4,10 +4,9 @@ NUMBER=$1
 
 # -gt, -le, -eq, -ge, -le
 
-if [$NUMBER -gt 100]
+if [ $NUMBER -gt 100 ]
 then
     echo " given number is greater than 100"
-
 else
     echo "given number is less than 100"
 fi 
